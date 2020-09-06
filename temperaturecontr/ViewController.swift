0db887e20578698ab03499f69ff90e2a810234e2
AlyJ <alina.jarmolica2@gmail.com>
2020-09-06 20:43:10 +0300

    Initial Commit

---
+++ temperaturecontr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  temperaturecontr
+//
+//  Created by alina.jarmolica on 06/09/2020.
+//  Copyright © 2020 alina.jarmolica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
